211c0c4050711ccea95df8383b9329fd0e79ca52
AbeerAl-Rafati <Abeerw.rafati@yahoo.com>
2021-05-17 10:09:26 +0300

    adding login on the header

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-                <LoginButton />
+                {/* <LoginButton /> */}
                 <MyFavoriteBooks />
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import LogoutButton from './LogoutButton';
+import LoginButton from './LoginButton';
 import './Header.css';
 
+
 class Header extends React.Component {
+
   render() {
+    // const { isAuthenticated } = this.props.auth0;
     return (
       <Navbar style={{ display: 'flex', justifyContent: 'space-between', alignContent: 'left' }} collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
@@ -14,11 +19,11 @@ class Header extends React.Component {
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
           {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
-          <LogoutButton />
+          {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
         </>
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
